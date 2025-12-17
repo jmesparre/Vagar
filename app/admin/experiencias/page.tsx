363ce3975e721526365bd16556afc0ae7056a92e
portfolio-web <jmesparre@gmail.com>
2025-12-17 12:22:16 -0300

    fix minor bug

--- app/admin/experiencias/page.tsx
+++ app/admin/experiencias/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import ExperiencesTable from '@/components/custom/ExperiencesTable';
 import { fetchAllExperiences } from '@/lib/data';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ExperiencesPage() {
   const experiences = await fetchAllExperiences();
 
